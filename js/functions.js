96686b9b205020df830c2aa6a2e9245043a80951
EduardoRafa1 <eduardorafael.01_@hotmail.com>
2021-01-30 20:04:48 -0300

    adicao de novos arquivos

--- js/functions.js
+++ js/functions.js
@@ -4,17 +4,34 @@ $(function(){
 	
 	
 	var val = 1;
+	var inicioDoCadastro = false;
 	var buttonNum = 0;
 	var buttonValores = ['Registrar','Avançar','Concluir'];
 	var botao = $('#registre-se p').html(buttonValores[buttonNum]);
 
 	$('#registre-se').click(function(){
 		
-			
+		if(inicioDoCadastro == true){
 			if (primeiraValidaCampo() == true) {
 				
-				val +=1;
+				val = 2;
+				
 			}
+			if(segundaValidaCampo() == true){
+				val = 3;
+			}
+			
+			if(terceiraValidaCampo() == true){
+				val = 4;
+			}
+			
+			if(quartaValidaCampo() == true){
+				console.log('Concluido');
+				alert('Registro concluido com sucesso!');
+			}
+
+		}
+			inicioDoCadastro = true;
 	
 	
 		if(val == 1){
@@ -54,6 +71,7 @@ $(function(){
 
 	//--------------- Avançar Campo -----------------------//
 	function primeiraValidaCampo(){
+
 		var razaoSocial = $('input[name=razaoSocial]').val();
 		if(razaoSocial == ''){
 			aplicarCampoInvalido($('input[name=razaoSocial'));
@@ -98,6 +116,78 @@ $(function(){
 		}
 	}
 
+	//---------Segunda Validação do campo----//
+
+	function segundaValidaCampo(){
+		if(verificaCPF() == false){
+			aplicarCampoInvalido($('input[name=cpf]'));
+			return false;
+		}if(verificaBirth() == false){
+			aplicarCampoInvalido($('input[name=birth]'));
+			return false;
+		}if(verificaNomeDaMae() == false){
+			aplicarCampoInvalido($('input[name=namemom]'));
+			return false;
+		}
+
+
+
+		else{
+			return true;
+		}
+	}
+
+	// ---- Terceira Valida Campo --- //
+	function terceiraValidaCampo(){
+		if(verificaNomeTitular() == false){
+			aplicarCampoInvalido($('input[name=nometitular]'));
+			return false;
+			
+		}if(verificaBanco()  == false){
+			aplicarCampoInvalido($('input[name=banco]'));;
+			return false;
+		}
+
+		if(verificaAgencia() == false){
+			aplicarCampoInvalido($('input[name=ag]'));
+			return false;
+		}if(verificaConta() == false){
+			aplicarCampoInvalido($('input[name=conta]'));
+			return false;
+		}
+
+		else{
+			return true;
+		}
+	}
+	// --- Quarta Valida Campo ---//
+
+	function quartaValidaCampo(){
+		if(verificaFoto() == false){
+			aplicarCampoInvalido($('input[name=foto]'));
+			return false;
+
+		}if(verificaFoto1() == false){
+			aplicarCampoInvalido($('input[name=foto1]'));
+			return false;
+		}
+		if(verificaFoto2() == false){
+			aplicarCampoInvalido($('input[name=foto2]'));
+			return false;
+		}
+		if(verificaContratoSocial() == false){
+			aplicarCampoInvalido($('input[name=contratoSocial]'));
+			return false;
+		}
+		if(verificaFoto3() == false){
+			aplicarCampoInvalido($('input[name=foto3]'));
+			return false;
+		}
+
+		else{
+			return true;
+		}
+	}
 
 	//--verifica os valores corretamente pra poder dar continuidade no preenchimento---/
 	function verificarCNPJ(){
@@ -192,6 +282,120 @@ $(function(){
 			return false;
 		}
 	}
+
+
+	//----Verificando os valores da Segunda Página -----//
+
+	function verificaCPF(){
+		var cpf = $('input[name=cpf]').val();
+		if(cpf == '')
+			return false;
+
+		if(!cpf.match(/^([0-9]{11})$/)){
+			return false;
+		}
+	}
+	function verificaBirth(){
+		var birth = $('input[name=birth]').val();
+		if(birth == ''){
+			return false;
+		}
+	}
+	function verificaNomeDaMae(){
+		var nomeDaMae = $('input[name=namemom]').val();
+		if(nomeDaMae == '')
+			return false;
+		
+		var amount = nomeDaMae.split(' ').length;
+		var splitStr = nomeDaMae.split(' ');
+
+		if(amount >=2){
+			for(var i = 0; i < amount ; i++){
+				if(splitStr[i].match(/^([A-z]{1}[a-z]{1,})$/)){
+					return true;
+					console.log('OK');
+				}else{
+					return false;
+				}
+			}
+		}else{
+			return false;
+		}
+	}
+
+	// -- Verificando a terceira pagina de cadastro -- //
+
+
+	function verificaNomeTitular(){
+		var nomeTitular = $('input[name=nometitular]').val();
+
+		if(nomeTitular == '')
+			return false;
+	}
+	function verificaBanco(){
+		var banco = $('input[name=banco]').val();
+
+		if(banco == '')
+			return false;
+	}
+	function verificaAgencia(){
+		var agencia = $('input[name=ag]').val();
+
+		if(agencia == '')
+			return false;
+
+		if(!agencia.match(/^([0-9]{4})$/)){
+			return false;
+		}
+	}
+	function verificaConta(){
+		var conta = $('input[name=conta]').val();
+
+		if(conta == '')
+			return false;
+
+		if(!conta.match(/^([0-9]{4})$/))
+			return false;
+	}
+
+	// --- Verificando A Quarta e Ultima Pagina --- //
+	function verificaFoto(){
+		var foto = $('input[name=foto]').val();
+
+		if(foto == ''){
+			return false;
+		}
+	}
+	function verificaFoto1(){
+		var foto1 = $('input[name=foto1]').val();
+
+		if (foto1 == '') 
+			return false;
+		
+	}
+	function verificaFoto2(){
+		var foto2 = $('input[name=foto2]').val();
+
+		if (foto2 == '') 
+			return false;
+		
+	}
+	function verificaContratoSocial(){
+		var contratoSocial = $('input[name=contratoSocial]').val();
+
+		if(contratoSocial == ''){
+			return false;
+		}
+	}
+	function verificaFoto3(){
+		var foto3 = $('input[name=foto3]').val();
+
+		if (foto3 == '') 
+			return false;
+		
+	}
+	
+	
 	
 
 	
@@ -201,6 +405,7 @@ $(function(){
 	function aplicarCampoInvalido(el){
 		el.css('border','2px solid red');
 	}
+
 
 	//---------Reseta Campo--------//
 	function resetarCampoInvalido(el){
